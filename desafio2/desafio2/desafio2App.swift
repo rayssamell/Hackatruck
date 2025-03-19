3fff4c4e424504275fe5be860f011ca3e7697194
Rayssa Mell <60720014+rayssamell@users.noreply.github.com>
2025-03-19 16:32:45 -0300

    desafio02

---
+++ desafio2/desafio2/desafio2App.swift
@@ -0,0 +1,17 @@
+//
+//  desafio2App.swift
+//  desafio2
+//
+//  Created by Turma01-26 on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct desafio2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
